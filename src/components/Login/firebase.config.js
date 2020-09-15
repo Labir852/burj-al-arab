35ff57324d43745d4faf9ba1bf429f974c07575b
Tanvir Anjum Labir <33982784+Labir852@users.noreply.github.com>
2020-09-15 09:23:53 +0600

    Added Authentication

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB-Cz6HD1O6KiIIHWmoBQjEH4q3GJmKynY",
+    authDomain: "burj-al-arab-b4d47.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-b4d47.firebaseio.com",
+    projectId: "burj-al-arab-b4d47",
+    storageBucket: "burj-al-arab-b4d47.appspot.com",
+    messagingSenderId: "191234124044",
+    appId: "1:191234124044:web:993a44a2893234aca98129"
+  };
+  export default firebaseConfig;
